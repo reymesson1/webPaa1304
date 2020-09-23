9498d11021872b4251e04bad84ae7e0b81de1ad8
reymesson1 <reymesson1>
2020-09-22 23:50:57 -0400

    diagonal horizontal working fine

--- my-app/src/app/dashboard/dashboard.component.ts
+++ my-app/src/app/dashboard/dashboard.component.ts
@@ -43,17 +43,7 @@ export class DashboardComponent implements OnInit {
     }
 
 
-    // for(var x=0;x<72;x++){
-
-    //     if(this.users[parseInt(x)].color =="red" && this.users[parseInt(x+1)].color == "red" &&  this.users[parseInt(x+2)].color == "red" && this.users[parseInt(x+3)].color == "red"){
-
-    //       this.openDialog();
-    //     }
-
-    // }
-
     // //vertical
-
     for(let x=0;x<12;x++){
 
       for(let y=0;y<3;y++){
@@ -61,7 +51,7 @@ export class DashboardComponent implements OnInit {
         // console.log( x + " " + y + " " + parseInt(y+1) + " " + parseInt(y+2) + " " + parseInt(y+3)  );
 
         console.log(this.columns[x].rows[y].color=="red"&&this.columns[x].rows[y+1].color=="red"&&this.columns[x].rows[y+2].color=="red"&&this.columns[x].rows[y+3].color=="red");
-        
+
         if(this.columns[x].rows[y].color=="red"&&this.columns[x].rows[y+1].color=="red"&&this.columns[x].rows[y+2].color=="red"&&this.columns[x].rows[y+3].color=="red"){
 
           this.openDialog();
@@ -70,19 +60,63 @@ export class DashboardComponent implements OnInit {
       }
     }
 
-    // for(var x=parseInt(this.temp)-1,z=0;x<72;x+=12,z++){
-
-    //   if(z<3){
-            
-    //     if(this.users[parseInt(x)].color=="red" && this.users[parseInt(x+12)].color=="red" && this.users[parseInt(x+24)].color=="red" && this.users[parseInt(x+36)].color=="red" ){
-
-    //       this.openDialog();          
-    //     }
-    //   }
-
-    // }
-
     // //diagonal horizontal
+    // for(let x=1;x<2;x++){
+    //   for(let y=1;y<2;y++){
+    //     console.log( parseInt(x) + "-"+ parseInt(y) + " " + parseInt(x+1)  + "-" + parseInt(y+1) + " " + parseInt(x+2) + "-" + parseInt(y+2) + " " + parseInt(x+3) + "-" + parseInt(y+3));
+    //   }
+    // }
+    // var x=0;
+    // var y=0;
+
+    // console.log( parseInt(x) + "-"+ parseInt(y) + " " + parseInt(x+1)  + "-" + parseInt(y+1) + " " + parseInt(x+2) + "-" + parseInt(y+2) + " " + parseInt(x+3) + "-" + parseInt(y+3));
+    // console.log( parseInt(x+1) + "-"+ parseInt(y+1) + " " + parseInt(x+1+1)  + "-" + parseInt(y+1+1) + " " + parseInt(x+2+1) + "-" + parseInt(y+2+1) + " " + parseInt(x+3+1) + "-" + parseInt(y+3+1));
+    // console.log( parseInt(x+1+1) + "-"+ parseInt(y+1+1) + " " + parseInt(x+1+1+1)  + "-" + parseInt(y+1+1+1) + " " + parseInt(x+2+1+1) + "-" + parseInt(y+2+1+1) + " " + parseInt(x+3+1+1) + "-" + parseInt(y+3+1+1));
+    
+    // console.log(" ");
+    // //suma uno solo columna -x 
+    // console.log( parseInt(x+1) + "-"+ parseInt(y) + " " + parseInt(x+1+1)  + "-" + parseInt(y+1) + " " + parseInt(x+2+1) + "-" + parseInt(y+2) + " " + parseInt(x+3+1) + "-" + parseInt(y+3));
+
+    //me queda pendiente 0-2 y 0-1 | 7-0 y 8-0
+    
+    for(let x=0;x<7;x++){
+      for(let y=0;y<3;y++){
+
+          // console.log( parseInt(x) + "-"+ parseInt(y) + " " + parseInt(x+1)  + "-" + parseInt(y+1) + " " + parseInt(x+2) + "-" + parseInt(y+2) + " " + parseInt(x+3) + "-" + parseInt(y+3));
+        if(y==0){
+          
+          console.log( parseInt(x) + "-"+ parseInt(y) + " " + parseInt(x+1)  + "-" + parseInt(y+1) + " " + parseInt(x+2) + "-" + parseInt(y+2) + " " + parseInt(x+3) + "-" + parseInt(y+3));
+          if(this.columns[x].rows[y].color=="red"&&this.columns[x+1].rows[y+1].color=="red"&&this.columns[x+2].rows[y+2].color=="red"&&this.columns[x+3].rows[y+3].color=="red"){
+
+            this.openDialog();
+          }
+        }else if(y==1){
+          
+          console.log( parseInt(x+1) + "-"+ parseInt(y) + " " + parseInt(x+2)  + "-" + parseInt(y+1) + " " + parseInt(x+3) + "-" + parseInt(y+2) + " " + parseInt(x+4) + "-" + parseInt(y+3));
+          if(this.columns[x+1].rows[y].color=="red"&&this.columns[x+2].rows[y+1].color=="red"&&this.columns[x+3].rows[y+2].color=="red"&&this.columns[x+4].rows[y+3].color=="red"){
+
+            this.openDialog();
+          }
+
+
+        }else if(y==2){
+          
+          console.log( parseInt(x+2) + "-"+ parseInt(y) + " " + parseInt(x+3)  + "-" + parseInt(y+1) + " " + parseInt(x+4) + "-" + parseInt(y+2) + " " + parseInt(x+5) + "-" + parseInt(y+3));
+          if(this.columns[x+2].rows[y].color=="red"&&this.columns[x+3].rows[y+1].color=="red"&&this.columns[x+4].rows[y+2].color=="red"&&this.columns[x+5].rows[y+3].color=="red"){
+
+            this.openDialog();
+          }
+
+
+        }
+
+
+      }
+
+      console.log(" ");
+    }
+
+
     // // for(var x=parseInt(this.temp),z=0;x<72;x+=13,z++){
 
     // //   if(z<3){
