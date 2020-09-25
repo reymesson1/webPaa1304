6e41f28594c3323177b7ae46ed76a8351012c604
reymesson1 <reymesson1>
2020-09-25 12:55:45 -0400

    audio

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -8,20 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   bool = false;
+  audio = new Audio("https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3");
   
   
   
   play(){
 
-    var audio = new Audio("https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3");
 
     if(!this.bool){
 
-      audio.play();
+      this.audio.play();
       this.bool = !this.bool;
       console.log("played");
     }else{
-      audio.pause();
+      this.audio.pause();
       this.bool = !this.bool;
       console.log("paused");
     }
